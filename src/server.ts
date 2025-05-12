df7bbc5d2d17bbbb8d9270d7c3504ca81312924d
dineshbillava912 <dineshbillava91.2@gmail.com>
2025-05-12 16:07:48 +0530

    Updated server.ts

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,10 @@ app.use(cors({
 })); */
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/i3')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/your-local-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log('MongoDB connected'))
   .catch((error) => console.error('MongoDB connection error:', error));
 
